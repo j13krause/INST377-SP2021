c57a9100cdd3e8d7840f790b0fb3c53483b3532f
j13krause <51761305+j13krause@users.noreply.github.com>
2021-02-12 21:06:24 -0500

    Wrote the entire script

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,29 @@
-/* Put your javascript in here */
+const listContainer = carousel.querySelector(".images")
+
+function moveImages_right(){
+    const firstImages = Array.from(listContainer.children).slice(0,3);
+    firstImages.forEach((element) => {
+        listContainer.removeChild(element);
+        listContainer.appendChild(element);
+    });
+}
+
+function moveImages_left(){
+    const lastImages = Array.from(listContainer.children).slice(4,7).reverse();
+    lastImages.forEach((element) => {
+        listContainer.removeChild(element);
+        listContainer.insertBefore(element, listContainer.children[0]);
+    });
+}
+
+
+function startPage(){
+    carousel.querySelector("button.arrow.prev").addEventListener("click", (event) => {
+       moveImages_left();
+    });
+    carousel.querySelector("button.arrow.next").addEventListener("click", (event) => {
+       moveImages_right();
+    });
+  
+}
+window.onload = startPage;
