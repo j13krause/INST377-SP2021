f391ea807f3df45491f3bc29d90d594d77c8151b
j13krause <51761305+j13krause@users.noreply.github.com>
2021-02-18 23:35:37 -0500

    wrote the files and tested in local host, now checking against cypress through heroku

--- server.js
+++ server.js
@@ -43,7 +43,9 @@ app.route('/api')
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
     console.log('Now send something back to your client');
-    // res.json({data: dataToSendToFrontEnd});
+    // res.json({message: 'Hello World'});
+    res.send('Hello World')
+    console.log('hello123')
   });
 
 app.listen(port, async () => {
